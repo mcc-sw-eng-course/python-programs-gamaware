3a657aea4f80de872ed61b63915ecff317b61b68
Alex Garcia <37558589+gamaware@users.noreply.github.com>
2019-02-11 20:09:49 -0600

    Modified readme file

--- readme.py
+++ readme.py
@@ -1,5 +1,5 @@
 # Course: TC4002 Analysis, Design and Construction of Software Systems
-# Enrollment: A00354776, A01223463
+# Enrollment IDs: A00354776, A01223463
 # Authors: Alex Garcia, Bruno Vergaray
 # Description: Repo with python code excercises
 # Date created: 02/11/2019
