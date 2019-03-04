ae21958544050815d7a81701e08f78f8919251b5
Alex Garcia <alex@gamaware.io>
2019-03-04 01:08:17 -0600

    Removing some unnecesary files

--- readme.py
+++
@@ -1,7 +0,0 @@
-# Course: TC4002 Analysis, Design and Construction of Software Systems
-# Enrollment IDs: A00354776, A01223463
-# Authors: Alex Garcia, Bruno Vergaray
-# Description: Repo with python code excercises
-# Date created: 02/11/2019
-# Date last modified: 02/11/2019
-# Python Version:  3.7.2
