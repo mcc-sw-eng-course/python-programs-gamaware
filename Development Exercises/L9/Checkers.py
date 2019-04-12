a70fe3975a2cbf73f93163a16b8ecca13bd84deb
Alex Garcia <37558589+gamaware@users.noreply.github.com>
2019-04-11 20:49:51 -0500

    Adding checkers game

---
+++ Development Exercises/L9/Checkers.py
@@ -0,0 +1,11 @@
+# Course: TC4002 Analysis, Design and Construction of Software Systems
+# Enrollment: A01223463, A00354776 
+# Author: Bruno Vergaray, Alex Garcia
+# Excercise: L9
+# File name: Checkers.py
+# Description: Checkers Game
+# Date created: 04/07/2019
+# Date last modified: 04/07/2019
+# Python Version:  3.7.2
+
+# Begin code
